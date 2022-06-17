84ea9bcb30a31fe5df7956a2abf75e8b4aa465b8
Juan Jiménez <nefioxdev@gmail.com>
2022-06-16 21:56:17 -0500

    Fixes & some code refactor

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,9 @@
 import Head from "next/head";
-import {PostCard, Categories, PostWidget, Loader} from '../components';
-import { getPosts } from '../services';
-import { FeaturedPosts } from '../sections';
+import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
+import { FeaturedPosts } from "../sections";
 
 export default function Home({ posts }) {
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <Loader />;
-  }
-
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -19,25 +13,25 @@ export default function Home({ posts }) {
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => <PostCard post={post.node} key={post.title} />).reverse()}
+          {posts
+            .map((post) => <PostCard post={post.node} key={post.title} />)
+            .reverse()}
         </div>
-      
-      <div className="lg:col-span-4 col-span-1">
-        <div className="lg:sticky relative top-8">
-          <PostWidget />
-          <Categories />
+
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
-};
-
+}
 
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
   return {
     props: { posts },
-    fallback: true,
-  }
+  };
 }
