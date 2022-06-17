312ad109e37576b96d574c1de8d4aa6108bd2acc
Juan Jiménez <nefioxdev@gmail.com>
2022-06-16 19:56:44 -0500

    Adjacent post card & categories fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import {PostCard, Categories, PostWidget} from '../components';
 import { getPosts } from '../services';
-
-
+import { FeaturedPosts } from '../sections';
 
 const Home: NextPage = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>CMS Blog</title>
+        <title>Developer Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useRouter } from "next/router";
 import { getPosts, getPostDetails } from "../../services";
 import {
   PostDetail,
@@ -8,15 +8,24 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader
 } from "../../components";
+import { AdjacentPosts } from '../../sections';
 
 function PostDetails({ post }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+  
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           <PostDetail post={post}/>
           <Author author={post.author}/>
+          <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
           <CommentsForm slug={post.slug}/>
           <Comments slug={post.slug}/>
         </div>
@@ -45,6 +54,6 @@ export async function getStaticPaths() {
 
   return {
     paths: posts.map(({ node : { slug }}) => ({ params: { slug }})),
-    fallback: false,
+    fallback: true,
   }
 }
